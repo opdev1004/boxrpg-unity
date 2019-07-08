e774afe91d909120e71b71c5917d90e1a9e7e327
nicholasrowley <nick-1.03@hotmail.com>
2019-07-08 20:07:03 +1200

    Added Player Movement
    - The Player can now move using the movement keys.
    - The Player can now jump using spacebar or A on the gamepad.

--- boxrpg/Assets/Scripts/PlayerMovement.cs
+++ boxrpg/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,84 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float turnSpeed = 20f; //turn speed in radians per second
+    public float movementSpeed = 8.0f;
+    public float jumpForce = 10.0f;
+    public List<KeyCode> jumpKey = new List<KeyCode>{ KeyCode.Space, KeyCode.JoystickButton0 };
+
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+    bool isJumping;
+    float jumpTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Rigidbody = GetComponent<Rigidbody>();
+        jumpTime = 0f;
     }
 
-    // Update is called once per frame
+    // Instead of being called before every rendered frame like Update(), FixedUpdate is called before the physics system solves any collisions and other interactions that have happened.  By default it is called exactly 50 times every second.
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        if (isWalking)
+        {
+            Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+            m_Rotation = Quaternion.LookRotation(desiredForward);
+
+            m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * movementSpeed * Time.deltaTime);
+
+            if (Physics.Raycast(m_Rigidbody.position, Vector3.down, 0.55f))
+            {
+                m_Rigidbody.MoveRotation(m_Rotation);
+            }
+        }
+
+        jump();
+    }
+
+    //runs every frame
     void Update()
     {
-        
+        //check if the player is standing on a solid object
+        if (Physics.Raycast(m_Rigidbody.position, Vector3.down, 0.55f))
+        {
+            foreach (KeyCode key in jumpKey)
+            {
+                if (Input.GetKeyDown(key))
+                {
+                    isJumping = true;
+                    jumpTime = 0.5f;
+                }
+            }
+        }
+    }
+
+    void jump()
+    {
+        if (isJumping)
+        {
+            if (jumpTime > 0)
+            {
+                Vector3 jumpvector = new Vector3(0, jumpForce, 0);
+                m_Rigidbody.MovePosition(m_Rigidbody.position + jumpvector * movementSpeed * Time.deltaTime);
+                jumpTime -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
     }
 }
