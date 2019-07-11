d35ed57ad8d48f75e75800b47d942767ce4e8ada
Victor Chanil Park <ncksdlf92@gmail.com>
2019-07-12 03:32:29 +1200

    Updated camera view

--- boxrpg/Assets/Scripts/Player/PlayerMovement.cs
+++ boxrpg/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,52 +4,50 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float turnSpeed = 20f; //turn speed in radians per second
-    public float movementSpeed = 8.0f;
-    public float jumpForce = 10.0f; //jump force per second
-    public List<KeyCode> jumpKey = new List<KeyCode>{ KeyCode.Space, KeyCode.JoystickButton0 };
-    public float jumpDuration = 0.5f; //jump duration in seconds
+	//object variables
+	public GameObject cameraObj;
+	Rigidbody camRigidbody;
+	Rigidbody m_Rigidbody;
+	BoxCollider m_BoxCollider;
 
-    Rigidbody m_Rigidbody;
-    BoxCollider m_BoxCollider;
-    Vector3 m_Movement;
-    Quaternion m_Rotation = Quaternion.identity;
+	//turn speed in radians per second
+    public float turnSpeed = 0f;
+    public float movementSpeed = 8.0f;
+
+	// jumping variables
+	public float jumpDuration = 0.5f;
+	public float jumpForce = 2.0f;
+	private List<KeyCode> jumpKey = new List<KeyCode>{ KeyCode.Space, KeyCode.JoystickButton0 };
     bool isJumping;
-    float jumpTime;
+
+    //Quaternion m_Rotation = Quaternion.identity;
+
+	// camera variables
+	public float camSpeedX = 1.0f;
+	public float camSspeedY = 1.0f;
+	// default offset distance is 5.0f
+	private float offsetDistance = 5.0f;
+	// Angle
+	private float r;
+	// holding player's position to update camera'
+	// private Vector3 previousPlayerPos;
+
 
     // Start is called before the first frame update
     void Start()
     {
+		camRigidbody = cameraObj.GetComponent<Rigidbody>();
         m_Rigidbody = GetComponent<Rigidbody>();
         m_BoxCollider = GetComponent<BoxCollider>();
-        jumpTime = 0f;
+		r = 0.0f;
     }
 
     // Instead of being called before every rendered frame like Update(), FixedUpdate is called before the physics system solves any collisions and other interactions that have happened.  By default it is called exactly 50 times every second.
     void FixedUpdate()
     {
-        //Check for movement
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        m_Movement.Set(horizontal, 0f, vertical);
-        m_Movement.Normalize();
-
-        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
-        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
-        bool isWalking = hasHorizontalInput || hasVerticalInput;
-
-        if (isWalking)
-        {
-            Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
-            m_Rotation = Quaternion.LookRotation(desiredForward);
-
-            m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * movementSpeed * Time.deltaTime);
-
-            m_Rigidbody.MoveRotation(m_Rotation);
-        }
-
-        Jump();
+		RotationWithCam();
+		Movement();
+		Jump();
     }
 
     //runs every frame
@@ -67,23 +65,99 @@ public class PlayerMovement : MonoBehaviour
                     if (Physics.BoxCast(m_Rigidbody.position, boxCastHalfExtents, Vector3.down, Quaternion.LookRotation(Vector3.down), m_BoxCollider.size.y * 0.05f))
                     {
                         isJumping = true;
-                        jumpTime = 0.5f;
+                        jumpDuration = 0.5f;
                     }
                 }
             }
         } 
     }
 
+
+	void RotationWithCam(){
+		// store mouse movement input value
+		float mouseX = camSpeedX * Input.GetAxis("Mouse X");
+		// float mouseY = speedW * Input.GetAxis("Mouse Y");
+
+		// calculate angle
+		r = r + mouseX;
+		if(r > 360){
+			r = 0.0f;
+		} else if(r < 0) {
+			r = 360.0f;
+		}
+
+		if(Input.GetAxis("Mouse ScrollWheel") > 0){
+			if(offsetDistance > 1.0f){
+				offsetDistance = offsetDistance - 0.5f;
+			}
+		}
+		if(Input.GetAxis("Mouse ScrollWheel") < 0){
+			if(offsetDistance < 7.1f){
+				offsetDistance = offsetDistance + 0.5f;
+			}
+		}
+
+		// Change player and camera's rotation
+		Vector3 rotationFactor = new Vector3 (0, r, 0);
+		Quaternion playerRotation = Quaternion.Euler(m_Rigidbody.rotation * rotationFactor);
+		m_Rigidbody.MoveRotation(playerRotation);
+
+		// calculate camera's new position
+		float camPosX = -Mathf.Sin(r * Mathf.Deg2Rad) * offsetDistance;
+		float camPosZ = -Mathf.Cos(r * Mathf.Deg2Rad) * offsetDistance;
+		float camPosY = camRigidbody.position.y;
+		Vector3 newCamPos = new Vector3 (transform.position.x + camPosX, camPosY, transform.position.z + camPosZ);
+
+		// change the position of camera
+		cameraObj.transform.position = newCamPos;
+		cameraObj.transform.LookAt(transform);
+	}
+
+
+	void Movement(){
+		// get input
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(horizontal, 0f, vertical);
+        movement.Normalize();
+
+		// check if input is happening
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+		// do if there's input
+        if (isWalking)
+        {
+			// Calculate the rotated vector value for movement
+			float movementX = movement.x * Mathf.Cos(transform.rotation.y * Mathf.Deg2Rad) - movement.z * Mathf.Sin(transform.rotation.y * Mathf.Deg2Rad);
+			float movementZ = movement.x * Mathf.Sin(transform.rotation.y * Mathf.Deg2Rad) + movement.z * Mathf.Cos(transform.rotation.y * Mathf.Deg2Rad);
+			Vector3 desiredMovement = new Vector3(movementX, movement.y, movementZ);
+            //Vector3 desiredForward = Vector3.RotateTowards(transform.forward, movement, turnSpeed * Time.deltaTime, 0f);
+            //m_Rotation = Quaternion.LookRotation(desiredForward);
+
+            m_Rigidbody.MovePosition(m_Rigidbody.position + desiredMovement * movementSpeed * Time.deltaTime);
+			/*
+            Vector3 desiredForward = Vector3.RotateTowards(transform.forward, movement, turnSpeed * Time.deltaTime, 0f);
+            m_Rotation = Quaternion.LookRotation(desiredForward);
+
+            m_Rigidbody.MovePosition(m_Rigidbody.position + movement * movementSpeed * Time.deltaTime);
+
+            m_Rigidbody.MoveRotation(m_Rotation);
+			*/
+        }
+	}
+
     //Moves the character up while it is jumping.
     void Jump()
     {
         if (isJumping)
         {
-            if (jumpTime > 0)
+            if (jumpDuration > 0)
             {
-                Vector3 jumpvector = new Vector3(0, jumpForce, 0);
-                m_Rigidbody.MovePosition(m_Rigidbody.position + jumpvector * movementSpeed * Time.deltaTime);
-                jumpTime -= Time.deltaTime;
+                Vector3 jumpVector = new Vector3(0, jumpForce, 0);
+                m_Rigidbody.MovePosition(m_Rigidbody.position + jumpVector * movementSpeed * Time.deltaTime);
+                jumpDuration -= Time.deltaTime;
             }
             else
             {
