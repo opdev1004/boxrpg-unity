5031af95be85b83f195b489a72f573ae06771c60
nicholasrowley <nick-1.03@hotmail.com>
2019-07-12 18:20:43 +1200

    Added player projectile to the game

---
+++ boxrpg/Assets/Scripts/Player/PlayerSkills.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkills : MonoBehaviour
+{
+    public GameObject projectileObj;
+    Rigidbody rigidbody;
+    private List<KeyCode> projectileSkillKey = new List<KeyCode> { KeyCode.Z, KeyCode.JoystickButton2 };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (KeyCode key in projectileSkillKey)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                FireProjectile();
+            }
+        }
+    }
+
+    void FireProjectile()
+    {
+        GameObject projectileObject = Instantiate(projectileObj, rigidbody.position + Vector3.forward, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(Vector3.forward, 900);
+    }
+}
---
+++ boxrpg/Assets/Scripts/Skills/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Projectile Collision with " + collision.gameObject);
+        Destroy(gameObject);
+    }
+ 
+    public void Launch(Vector3 direction, float force)
+    {
+        rigidbody.AddForce(direction * force);
+    }
+}
