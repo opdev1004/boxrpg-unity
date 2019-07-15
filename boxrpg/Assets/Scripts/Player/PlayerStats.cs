2c735dc3911217f383594a72b6158be88ae8a486
Nicholas Rowley <nick-1.03@hotmail.com>
2019-07-15 16:43:19 +1200

    Added player stats script

---
+++ boxrpg/Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This class handles storing and changing player stats
+public class PlayerStats : MonoBehaviour
+{
+    int health;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 5;
+    }
+
+    //adds the specified amount of health
+    public void AddHealth(int amount)
+    {
+        health += amount;
+    }
+}
